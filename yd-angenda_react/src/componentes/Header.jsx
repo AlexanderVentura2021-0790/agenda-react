0f4e935c7493ddc75bd1e13a21cfe4c4469c8b8f
Alexander Ventura <94939445+AlexanderVentura2021-0790@users.noreply.github.com>
2022-07-18 21:50:47 -0400

    Update Header.jsx

---
+++ yd-angenda_react/src/componentes/Header.jsx
@@ -0,0 +1,11 @@
+const Header = props => {
+  return (
+    <div className="col-sm-10">
+        <h1>tarea 4, agenda react</h1>
+        <p>Alexander Ventura</p>
+    </div>
+  )
+}
+
+
+export default Header
